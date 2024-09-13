927334b1754edf6d3bd4888505f7e5b1f11646ee
C <48510756+MrVWY@users.noreply.github.com>
2024-09-13 10:35:34 +0800

    Create distributedLocks.go

---
+++ 代码/distributedLocks.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "time"
+
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+    "go.mongodb.org/mongo-driver/mongo/readpref"
+)
